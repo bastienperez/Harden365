d45cb17438a42996362cc2ea5138c4a6c00406c3
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2024-01-25 19:25:39 +0100

    - Add Disable Tenant Creation - Prepare 'UsersPermissionToUserConsentToAppEnabled' to Graph

--- Mod/Harden365.Identity.psm1
+++ Mod/Harden365.Identity.psm1
@@ -1,10 +1,4 @@
-﻿
-
-
-
-
-
-Function Start-UserConsentToApp {
+﻿function Start-UserConsentToApp {
      <#
         .Synopsis
          Disable User permission consent App registration
@@ -19,11 +13,42 @@ Function Start-UserConsentToApp {
 
 #SCRIPT
 if ((Get-MsolCompanyInformation).UsersPermissionToUserConsentToAppEnabled  -eq $true) {
-Set-MsolCompanySettings -UsersPermissionToUserConsentToAppEnabled $false
-Write-LogInfo 'Disable User permission consent App registration' 
-}
-      
+     Set-MsolCompanySettings -UsersPermissionToUserConsentToAppEnabled $false
+     Write-LogInfo 'Disable User permission consent App registration' 
 }
 
+<# Disable User permission consent app with MSGraph - need review
+# we need to keep only the current ManagePermissionGrantsForOwnedResource.* policies
+# https://learn.microsoft.com/en-us/entra/identity/enterprise-apps/configure-user-consent?pivots=ms-powershell#disable-user-consent
+$currentPolicies = (Get-MgBetaPolicyAuthorizationPolicy).PermissionGrantPolicyIdsAssignedToDefaultUserRole
+
+$policyPrefixToKeep = "ManagePermissionGrantsForOwnedResource."
+
+$userConsentPolicy = 'ManagePermissionGrantsForSelf.microsoft-user-default-legacy'
+$currentPolicies = (Get-MgBetaPolicyAuthorizationPolicy).PermissionGrantPolicyIdsAssignedToDefaultUserRole
+
+# Cast to array
+$newPolicies = @($currentPolicies | Where-Object { $_ -notlike "$userConsentPolicy" })
+
+if($null -eq $newPolicies){
+	$body = @{permissionGrantPolicyIdsAssignedToDefaultUserRole=""}
+}
+else {
+	$body = @{
+		"permissionGrantPolicyIdsAssignedToDefaultUserRole" = $newPolicies
+	}
+}
+
+function Start-UserTenantCreation {
+
+Update-MgBetaPolicyAuthorizationPolicy -AuthorizationPolicyId 'authorizationPolicy' -BodyParameter $body
+#>
 
 
+     # Remove AllowedToCreateTenants permission - need review
+     <#
+     if((Get-MgPolicyAuthorizationPolicy).DefaultUserRolePermissions.AllowedToCreateTenants){
+          Update-MgPolicyAuthorizationPolicy -DefaultUserRolePermissions @{AllowedToCreateTenants = $false}
+     }
+     #>    
+}
